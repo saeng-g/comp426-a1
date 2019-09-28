f0dbffd3b60278683037c72432710142453937b9
Ky Kim <kyungjinkim94@gmail.com>
2019-09-28 16:04:40 -0400

    balls + moving
    error: balls not moving properly (reflection is wrong; change dir is not nuanced); not multithreaded.

--- ball.c
+++ ball.c
@@ -9,7 +9,7 @@ void set_gravity(float g) {
 }
 
 // moves ball's position based on its velocity
-void move(ball* b) {
+void move_ball(ball* b) {
     b->p.x = b->p.x + b->v.vx;
     b->p.y = b->p.y + b->v.vy;
 }
@@ -27,7 +27,7 @@ void change_velocity(ball* b) {
         b->v.vx = b->v.vx + FRICTION_CONSTANT_X;
     }
     //change vy
-    b->v.vy = b->v.vy == 0 ? 0 : b->v.vy + GRAVITY_CONSTANT_Y;
+    b->v.vy = (b->v.vy <= 0.001f) && (b->p.y - b->w <= -0.999f) ? 0 : b->v.vy - GRAVITY_CONSTANT_Y;
 }
 
 void change_direction(ball* b) {
@@ -37,68 +37,126 @@ void change_direction(ball* b) {
 
 // returns 1 if balls are touching else 0
 int touch(ball* b1, ball* b2) {
-    double x_diff = pow((double) b1->p.x, 2);
-    double y_diff = pow((double) b1->p.y, 2);
+    double x_diff = pow((double) b1->p.x - (double) b2->p.x, 2);
+    double y_diff = pow((double) b1->p.y - (double) b2->p.y, 2);
     double dist = sqrt(x_diff + y_diff);
     return ((int)dist <= (b1->w + b2->w));
 }
 
 // returns 1 if ball touches wall else 0
-int touch_wall(ball* b1, float minx, float maxx, float miny, float maxy) {
-    int cond_x = (b1->p.x - b1->w <= minx) && (b1->p.x + b1->w >= maxx);
-    int cond_y = (b1->p.y - b1->w <= miny) && (b1->p.y + b1->w >= maxy);
-    return cond_x && cond_y;
+char touch_wall(ball* b1, float minx, float maxx, float miny, float maxy) {
+    if (b1->p.x - b1->w <= minx) { return 'l'; } //left wall
+    else if (b1->p.x + b1->w >= maxx) { return 'r'; } //right wall
+    else if (b1->p.y - b1->w <= miny) { return 'b'; } //bottom wall
+    else if (b1->p.y + b1->w >= maxy) { return 't'; } //top wall
+    else { return '0'; } //not touching any walls
 }
 
+// ball generation
 point get_new_point() {
-    point p = {(float)(rand()%500+1), (float)(rand()%500+1)};
+    point p = {(float)(rand()%1000+1-500)/1000.0f, (float)(rand()%1000+1-500)/1000.0f};
     return p;
 }
-
 color get_new_color() {
     int i = rand()%3 + 1;
     if (i == 1) {
-        color c = {0.0f, 0.0f, 255.0f};
+        color c = {0.0f, 0.0f, 1.0f};
         return c;
     }
     else if (i == 2) {
-        color c = {0.0f, 255.0f, 0.0f};
+        color c = {0.0f, 1.0f, 0.0f};
         return c;
     }
     else {
-        color c = {2.55f, 0.0f, 0.0f};
+        color c = {1.0f, 0.0f, 0.0f};
         return c;
     }
 }
-
 velocity get_new_velocity() {
-    velocity v = {(float)(rand()%10+1), (float)(rand()%10+1)};
+    velocity v = {(float)(rand()%40+1-20)/1000.0f, (float)(rand()%40+1-20)/1000.0f};
     return v;
 }
-
 float get_new_weight() {
     int i = rand()%3 + 1;
-    return (float)i*50.0f;
+    return (float)i*50.0f/1000.0f;
 }
-
 ball get_new_ball() {
     ball b = {get_new_point(), get_new_color(), get_new_velocity(), get_new_weight()};
     return b;
 }
 
-int main() {
-    time_t t;
-    srand((unsigned) time(&t));
+int nb_balls = 5; //rand()%7 + 3;
+ball* balls = (ball*) malloc(nb_balls * sizeof(ball));
 
-    int nb_balls = rand()%7 + 3;
-    printf("nb of balls generated: %d\n", nb_balls);
-    ball* balls = (ball*) malloc(nb_balls * sizeof(ball));
+void draw_ball(ball* b) {
+    float x = b->p.x;
+    float y = b->p.y;
+    float r = b->w;
+    glColor4f(b->c.r, b->c.g, b->c.b, 0.7f);
+    glBegin(GL_TRIANGLES);
+    printf("%f,%f,%f\n", x, y, r);
+    int nb_vertices = 32;
+    for (int i=0; i<nb_vertices; i++) {
+        float theta = 2.0f*3.14159 / (float)nb_vertices;
+        glVertex2f(x, y);
+        glVertex2f(x+(r*sinf(theta*i)), y+(r*cosf(theta*i)));
+        glVertex2f(x+(r*sinf(theta*(i+1))), y+(r*cosf(theta*(i+1))));
+    }
+    glEnd();
+}
+
+void timer(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(30, timer, 0);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(1, 1, 1, 1);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    printf("hi");
+    for (int i=0; i<nb_balls; i++) {
+        printf("%f,%f,%f\n", (balls+i)->p.x, (balls+i)->p.y, (balls+i)->w);
+        draw_ball(balls+i);
+    }
+    for (int i=0; i<nb_balls; i++) {
+        move_ball(balls+i);
+        printf("ball moved%d\n", i);
+    }
+    for (int i=0; i<nb_balls; i++) {
+        char a = touch_wall(balls+i, -1.0f, 1.0f, -1.0f, 1.0f);
+        for (int j=i; j<nb_balls; j++) {
+            if (i!=j) {
+                int b = touch(balls+i, balls+j);
+                printf("b is %d\n", b);
+                if (b) {
+                    printf("changedir");
+                    change_direction(balls+i);
+                }
+            }
+        }
+        if (a!='0') {
+            change_direction(balls+i);
+        }
+        change_velocity(balls+i);
+    }
+    glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
     for (int i=0; i<nb_balls; i++) {
         *(balls + i) = get_new_ball();
+        printf("ball%d completed\n", i);
     }
-
-    for (int i =0; i<nb_balls; i++) {
-        printf("ball%d : p.x=%f, p.y=%f\n", i, (balls + i)->p.x, (balls + i)->p.y);
-    }
+    srand((unsigned) time(NULL));
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(1000, 1000);
+    glutCreateWindow("comp426_a1: watch me bounce my balls");
+    glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
+    glutMainLoop();
     return 0;
 }
--- ball.h
+++ ball.h
@@ -3,8 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-float FRICTION_CONSTANT_X = 0; //default friction
-float GRAVITY_CONSTANT_Y = 1; //default gravity
+// OpenGL related libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <OpenGL/OpenGL.h>
+#include <GL/freeglut.h>
+
+float FRICTION_CONSTANT_X = 0.00001f; //default friction
+float GRAVITY_CONSTANT_Y = 0.00001f; //default gravity
 
 typedef struct {
     float r, b, g;
@@ -28,14 +34,14 @@ typedef struct {
 void set_friction(float);
 void set_gravity(float);
 
-void move(ball*);
+void move_ball(ball*);
 void change_velocity(ball*);
 void change_direction(ball*);
 int touch(ball*, ball*);
-int touch_wall(ball*, float, float, float, float);
+char touch_wall(ball*, float, float, float, float);
 
-point get_new_point();
-color get_new_color();
-velocity get_new_velocity();
-float get_new_weight();
-ball get_new_ball();
+point get_new_point(void);
+color get_new_color(void);
+velocity get_new_velocity(void);
+float get_new_weight(void);
+ball get_new_ball(void);
